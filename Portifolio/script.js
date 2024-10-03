8ab13fdd56cdb492084aca90f1a7e7a0d6fb8e94
Daniel Igor <xdaniel.igor@gmail.com>
2024-10-03 14:07:11 -0300

    Criação do portifolio
    Ainda falta implementação!
    Testando a parte responsiva do site

---
+++ Portifolio/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typewriter-text",
+    {
+        strings: ["DEVELOPER","FREELANCER", "DESIGNER"],
+        typeSpeed:100,
+        backSpeed:50,
+        loop: true
+    }
+)
